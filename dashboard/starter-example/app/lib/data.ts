08c2b2ff093a2d2ccbff030cfcbba3a0bc8c99de
ankit gupta <ankit.gupta@cheppers.com>
2024-01-18 19:31:04 +0530

    Remove console.log msg.

--- dashboard/starter-example/app/lib/data.ts
+++ dashboard/starter-example/app/lib/data.ts
@@ -20,13 +20,7 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -39,8 +33,7 @@ export async function fetchLatestInvoices() {
   noStore();
 
   try {
-    console.log('Fetching latest Invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 6000));
+    
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
